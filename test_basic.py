fdc188bdb6a304024ab5592ab0111086dc6821aa
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-21 22:18:18 +0300

    add test value.

--- test_basic.py
+++ test_basic.py
@@ -6,20 +6,16 @@ import xml.etree.ElementTree as ETr
 @pytest.mark.parametrize("amount", [5, 500])
 def test_success_payment(config, body, amount):
     """Boundary value test for success simple payment with mandatory parameters only."""
-    print(body)
-    response = requests.post(config['base_url'], data=str(body), verify=False)
+    response = requests.post(config['base_url'], data=body, verify=False)
     root = ETr.fromstring(response.content)
-    print(response.content)
     assert root.find('description').text != "opcode is absent!"
 
 
-@pytest.mark.parametrize("amount", [-1, 0, 4.99, 500.01])
+@pytest.mark.parametrize("amount", [-5, 0, 4.99, 500.01, 50000000000000000000000])
 def test_unsuccess_payment(config, body, amount):
     """Boundary value test for unsuccessful payment with mandatory parameters only."""
-    print(body)
-    response = requests.post(config['base_url'], data=str(body), verify=False)
+    response = requests.post(config['base_url'], data=body, verify=False)
     root = ETr.fromstring(response.content)
-    print(response.content)
     assert root.find('status').text == "KO"
 
 
