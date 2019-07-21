4dac7ac0d7156d0ce462a7a2778ccc604ec1b1b1
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-21 20:41:15 +0300

    fix comments.

--- test_basic.py
+++ test_basic.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ETr
 
 @pytest.mark.parametrize("amount", [5, 500])
 def test_success_payment(config, body, amount):
-    """Test of success simple payment with mandatory parameters only."""
+    """Boundary value test for success simple payment with mandatory parameters only."""
     print(body)
     response = requests.post(config['base_url'], data=str(body), verify=False)
     root = ETr.fromstring(response.content)
@@ -15,7 +15,7 @@ def test_success_payment(config, body, amount):
 
 @pytest.mark.parametrize("amount", [-1, 0, 4.99, 500.01])
 def test_unsuccess_payment(config, body, amount):
-    """Test of success simple payment with mandatory parameters only."""
+    """Boundary value test for unsuccessful payment with mandatory parameters only."""
     print(body)
     response = requests.post(config['base_url'], data=str(body), verify=False)
     root = ETr.fromstring(response.content)
